9eaf2b8b91b4c3c73ef26d384b722cb020baa9bb
tonim <lucas.rocha@dtidigital.com.br>
2022-02-12 18:51:55 -0300

    update cli for new spider

--- spider-cli.js
+++ spider-cli.js
@@ -1,13 +1,13 @@
 import { spider } from './spider.js'
 
+const url = process.argv[2]
+const nesting = Number.parseInt(process.argv[3], 10) || 1
 
-
-spider(process.argv[2], (err, filename, downloaded) => {
+spider(url, nesting, err => {
     if(err) {
         console.error('Something went wrong', {err})
-    } else if (downloaded) {
-        console.log(`Completed the download of ${filename}`)
-    } else {
-        console.log(`"${filename}" was already downloaded`)
+        process.exit(1)
     }
+
+    console.log('Download complete')
 })
