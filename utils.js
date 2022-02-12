fd5f4f63dd26d24a42e82367c941904fdbdc540f
tonim <lucas.rocha@dtidigital.com.br>
2022-02-12 18:51:23 -0300

    methods to help get page links

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { URL } from 'url'
 import slug from 'slug'
+import cheerio from 'cheerio'
 
 export function urlToFilename(url) {
     const parsedUrl = new URL(url)
@@ -18,4 +19,23 @@ export function urlToFilename(url) {
     }
 
     return filename
+}
+
+export const getPageLinks = (currentUrl, body) => {
+    //TODO cheerio is not getting the tags
+    return Array.from(cheerio.load(body)('a'))
+        .map((element => {
+            return getLinkUrl(currentUrl, element)
+        }))
+        .filter(Boolean)
+}
+
+
+const getLinkUrl = (currentUrl, element) => {
+    const parsedLink = new URL(element.attribs.href || '', currentUrl)
+    const currentParsedUrl = new URL(currentUrl)
+    if (parsedLink.hostname !== currentParsedUrl.hostname || !parsedLink.pathname) {
+        return null
+    }
+    return parsedLink.toString()
 }
